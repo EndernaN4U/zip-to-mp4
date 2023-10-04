e53552846e76008a04371cdf4dda54b8ab33ab5d
endernan4u <adryanender@gmail.com>
2023-10-04 12:31:04 +0200

    Working writing from buffer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use zip::read::ZipArchive;
 use std::io::prelude::*;
 use std::fs::File;
 
@@ -8,14 +7,19 @@ fn list_zip_contents(reader: impl Read + Seek) -> zip::result::ZipResult<()> {
     for i in 0..zip.len() {
         let mut file = zip.by_index(i)?;
         println!("Filename: {}", file.name());
-        std::io::copy(&mut file, &mut std::io::stdout());
+        std::io::copy(&mut file, &mut std::io::stdout()).expect("Copy error");
     }
 
     Ok(())
 }
 fn main() {
     let mut file = File::open("./test/test.zip").expect("Blagam no");
+    let mut buf = [0; 1024];
+    let err = file.read(&mut buf);
+    err.expect("Reading err");
 
-    list_zip_contents(file);
-    
+    let mut file2 = File::create("./test/test2.zip").expect("pls");
+    let err2 = file2.write_all(&buf);
+    err2.expect("Writeing err");
+    let _ = list_zip_contents(file2);
 }
